811fcd99e7ff8ce434d1bbe11b6643c7d9002563
Ojas <22b0965@iitb.ac.in>
2023-10-19 19:13:11 +0530

    neglig changes

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -1,10 +1,29 @@
 #include "receiver.h"
-
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
 int main() {
 
     Receiver rcv;
     sleep(5);
     std::string message = rcv.readIML();
-    std::cout << message;
+    
+
+    // Create a stringstream from the input string
+    std::istringstream ss(message);
+
+    std::vector<std::string> splitStrings;
+    std::string token;
+
+    // Split the string using the delimiter '#'
+    while (std::getline(ss, token, '#')) {
+        splitStrings.push_back(token);
+    }
+
+    // Now, splitStrings contains the individual substrings
+    
+
+    
     return 0;
 }
