14ff605dd751676f68a0b1aafa6171b4d353bf24
Ojas <22b0965@iitb.ac.in>
2023-11-11 21:49:52 +0530

    tried

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -8,6 +8,9 @@
 using namespace std;
 int main() {
     CustomMap<std::string, int> stockMap;  //gonna store stock name and price
+    CustomMap<std::string, int> buyMap;    //gonna store stock name and lowest rejected buy price
+    CustomMap<std::string, int> sellMap;   //gonna store stock name and highest rejected sell price
+
     Receiver rcv;
     
     sleep(5);
@@ -54,15 +57,27 @@ int main() {
         //here i am assuming p_i is the price of last stock order.
         else {
             int p_i = stockMap.get(stock);
-            if(p_i < p && bs=="b"){
+            if(p_i < p && bs=="b" && (p > sellMap.get(stock) || sellMap.contains(stock) == false)){
                 stockMap.set(stock, p);
                 cout<<stock<<" "<<price<<" "<<"s"<<endl;
             }
-            else if(p_i > p && bs=="s"){
+            else if(p_i > p && bs=="s" && (p < buyMap.get(stock) || buyMap.contains(stock) == false)){
                 stockMap.set(stock, p);
                 cout<<stock<<" "<<price<<" "<<"b"<<endl;
             }
-            else cout<<"No trade"<<endl;
+            else {
+                if(p_i >= p && bs=="b"){
+                    if(sellMap.get(stock) < p || sellMap.contains(stock) == false){
+                        sellMap.set(stock,p);
+                    }
+                }
+                else if(p_i <= p && bs=="s"){
+                    if(buyMap.get(stock) > p || buyMap.contains(stock) == false){
+                        buyMap.set(stock,p);
+                    }
+                }
+                cout<<"No trade"<<endl;
+            }
         }
 
         
