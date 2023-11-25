4d16359b5700300149d19af98b1a89bfd75009cc
TanishkaKabra <tanishkakabra8124@gmail.com>
2023-11-26 01:49:18 +0530

    Phase 1 Part 1 checked against piazza testcases, and iml read done without port fry

--- phase1/traderpart1.cpp
+++ phase1/traderpart1.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 int main() {
     CustomMap<std::string, int> stockMap;  //gonna store stock name and price
-    CustomMap<std::string, int> buyMap;    //gonna store stock name and lowest rejected buy price
-    CustomMap<std::string, int> sellMap;   //gonna store stock name and highest rejected sell price
+    CustomMap<std::string, int> buyMap;    //gonna store stock name and best (highest) rejected buy price 
+    CustomMap<std::string, int> sellMap;   //gonna store stock name and best (lowest) rejected sell price 
     bool broken = false;
     string firsthalfline;
     Receiver rcv;
@@ -63,7 +63,7 @@ int main() {
             price = tokens[1];
             bs = tokens[2];
 
-            if(stockMap.contains(stock) == false) {
+            if(stockMap.contains(stock) == false) {   //stock is not in the list
                 stockMap.insert(stock, p);
                 cout<< stock <<" "<<p<<" ";
                 if (bs == "b") cout <<"s"<<endl;
@@ -74,8 +74,99 @@ int main() {
             
             //here i am assuming p_i is the price of last stock order.
             else {
-                int p_i = stockMap.get(stock);
-                if(p_i < p && bs=="b" && (p > sellMap.get(stock) || sellMap.contains(stock) == false) ){
+                int p_i = stockMap.get(stock);          //p_i is last traded price; sell above p_i, buy below p_i
+                    //buy offer > p_i        
+                //TK STARTS WRITING
+                // let's check cancellation conditions
+                if(bs == "b"){
+                    if(!buyMap.contains(stock))
+                        buyMap.insert(stock, p);
+                    else if(p > buyMap.get(stock))
+                        buyMap.set(stock, p);
+                    else{
+                        cout << "No trade" << endl;   //cancelled
+                        continue;
+                    }
+
+                }
+                else if(bs == "s"){
+                    if(!sellMap.contains(stock))
+                        sellMap.insert(stock, p);
+                    else if(p < sellMap.get(stock))
+                        sellMap.set(stock, p);
+                    else{
+                        cout << "No trade" << endl;   //cancelled
+                        continue;
+                    }
+                }
+
+                
+                if(bs == "b" && p == sellMap.get(stock) && sellMap.contains(stock)){ //if buy offer is equal to best rejected sell price
+                    cout<<"No trade"<<endl;
+                    sellMap.remove(stock);
+                    buyMap.remove(stock);
+                    continue;
+                }
+                else if(bs == "s" && p == buyMap.get(stock) && buyMap.contains(stock)){ //if sell offer is equal to best rejected buy price
+                    cout<<"No trade"<<endl;
+                    sellMap.remove(stock);
+                    buyMap.remove(stock);
+                    continue;
+                }
+
+                //aight so trade hasnt been cancelled
+                //cout << "Trade not yet cancelled   ";
+                if(bs == "b"){
+                    //trade is possible
+                    if(p > p_i){
+                        cout<<stock<<" "<<price<<" "<<"s"<<endl;
+                        stockMap.set(stock, p);
+                        buyMap.remove(stock);
+                        continue;
+                    }
+                    else{ //trade is not possible, see if p > best rejected buy price
+                        if(buyMap.get(stock) < p || buyMap.contains(stock) == false){
+                            buyMap.set(stock, p);
+                        }
+                        cout<<"No trade"<<endl;
+                        continue;
+                    }
+                }
+                else if(bs == "s"){
+                    //trade is possible
+                    if(p < p_i){
+                        cout<<stock<<" "<<price<<" "<<"b"<<endl;
+                        stockMap.set(stock, p);
+                        sellMap.remove(stock);
+                        continue;
+                    }
+                    else{ //trade is not possible, see if p < best rejected sell price
+                        if(sellMap.get(stock) > p || sellMap.contains(stock) == false){
+                            sellMap.set(stock, p);
+                        }
+                        cout<<"No trade"<<endl;
+                        continue;
+                    }
+                }
+                
+            }
+                //TK ENDS WRITING
+
+
+
+        }
+
+        if(eachline == "$"){
+            break;
+        }
+
+    }
+}
+
+
+
+//OJAS CODE : QUITE SIMILAR TO TK'S CODE, BUT DIDNT HAVE THE ENERGY TO LOOK FOR THE SUBTLETIES SO WROTE MY OWN AGAIN
+/*if((p_i < p && bs=="b") && (p > sellMap.get(stock) || sellMap.contains(stock) == false) ){
                     stockMap.set(stock, p);
 
                     if(buyMap.get(stock) != p){
@@ -114,12 +205,4 @@ int main() {
                 }
             }
 
-            //cout << eachline << endl;
-        }
-        if(eachline == "$"){
-            break;
-        }
-
-    }
-}
-
+            //cout << eachline << endl;*/
